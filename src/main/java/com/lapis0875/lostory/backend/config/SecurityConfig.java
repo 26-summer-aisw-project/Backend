fa9ab37f71fec2ced6e1064228d0e0af5469477b
Lapis0875 <lapis0875@gmail.com>
2026-07-08 14:54:38 +0900

    feat: health 엔드포인트 구현

---
+++ src/main/java/com/lapis0875/lostory/backend/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.lapis0875.lostory.backend.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+				.anyRequest().authenticated())
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable);
+		return http.build();
+	}
+
+}
