e99b25d149ce864caef7c41b0e900d93a23a786f
Lapis0875 <lapis0875@gmail.com>
2026-08-13 23:05:04 +0900

    feat: 감사 로그 모델 추가

---
+++ src/main/java/kr/lostory/backend/audit/domain/AuditLogRepository.java
@@ -0,0 +1,6 @@
+package kr.lostory.backend.audit.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
